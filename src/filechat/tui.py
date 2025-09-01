561f8fb9f50137157a7e345fdbd9c4bdadb35710
Milos Svana <milos.svana@mailfence.com>
2025-09-01 14:49:07 +0200

    Show history in the TUI

--- src/filechat/tui.py
+++ src/filechat/tui.py
@@ -1,10 +1,38 @@
 from textual import work
 from textual.app import App, ComposeResult
-from textual.containers import VerticalScroll
-from textual.widgets import Input, Static
+from textual.containers import Center, VerticalScroll, Vertical
+from textual.widgets import Input, ListItem, ListView, Static
+from textual.screen import ModalScreen
 
-from filechat.chat import Chat
+from filechat.chat import Chat, ChatStore
 from filechat.index import FileIndex
+
+
+class HistoryScreen(ModalScreen):
+
+    def __init__(self, chat_store: ChatStore, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._history_view = ListView()
+        self._chat_store = chat_store
+
+    def compose(self) -> ComposeResult:
+        with Vertical():
+            center = Center(self._history_view)
+            center.border_title = "Chat History"
+            yield center
+
+    def on_mount(self):
+        chats = self._chat_store.chat_list()
+        for chat in chats:
+            chat_item = ListItem(Static(chat[2]), Static(chat[1], classes="timestamp"))
+            self._history_view.append(chat_item)
+
+        if chats:
+            self._history_view.index = 0
+            self._history_view.focus()
+
+    def key_escape(self):
+        self.dismiss()
 
 
 class FilechatApp(App):
@@ -29,12 +57,30 @@ class FilechatApp(App):
         VerticalScroll {
             scrollbar-size: 0 0;
         }
+
+        Center {
+            background: $surface;
+            width: 60;
+            height: 50%;
+            min-height: 20;
+            border: heavy $primary;
+        }
+
+        Vertical {
+            align: center middle;
+            width: 100%;
+        }
+
+        Static.timestamp {
+            color: gray;
+        }
     """
 
-    def __init__(self, chat: Chat, index: FileIndex):
+    def __init__(self, chat: Chat, index: FileIndex, chat_store: ChatStore):
         super().__init__()
         self._chat = chat
         self._index = index
+        self._chat_store = chat_store
         self._chat_list = VerticalScroll()
         self._user_input = Input(placeholder="Enter chat message ... (or type /exit to quit)")
 
@@ -47,9 +93,11 @@ class FilechatApp(App):
     def on_input_submitted(self, event: Input.Submitted):
         if event.value.strip() == "/exit":
             self.exit()
-        if event.value.strip() != "":
+        elif event.value.strip() == "/history":
+            self._show_history_modal()
+        elif event.value.strip() != "":
             self.send_message(event.value)
-            self._user_input.value = ""
+        self._user_input.value = ""
 
     @work(thread=True)
     def send_message(self, message: str):
@@ -69,4 +117,8 @@ class FilechatApp(App):
             self.call_from_thread(output_widget.update, output_text)
             self.call_from_thread(self._chat_list.scroll_end)
 
+        self.call_from_thread(self._chat_store.store, self._chat)
         self.call_from_thread(self._user_input.set_loading, False)
+
+    def _show_history_modal(self):
+        self.push_screen(HistoryScreen(self._chat_store))
